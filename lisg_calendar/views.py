f09fcd78f93dbaf1cb57c672b5250b4fefee5257
tronze <0183535663@naver.com>
2019-02-16 05:47:12 +0900

    LikeLion Calendar > View: Initial commit

--- lisg_calendar/views.py
+++ lisg_calendar/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+
+from main_calendar.web_calendar import WebCalendar
 
 # Create your views here.
+
+
+def display_simple_web_calendar(request):
+    context = dict()
+    web_calendar = WebCalendar(year=2019)
+    context['calendar'] = mark_safe(web_calendar.print_calendar_table(3))
+    return render(request, 'lisg_calendar/main.html', context)

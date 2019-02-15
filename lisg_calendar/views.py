d8f91ae466588ce52c7a3f81a6bbf673adadf591
tronze <0183535663@naver.com>
2019-02-16 05:52:29 +0900

    LikeLion Calendar > View: Loading different month for calendar using GET Parameter

--- lisg_calendar/views.py
+++ lisg_calendar/views.py
@@ -9,5 +9,9 @@ from main_calendar.web_calendar import WebCalendar
 def display_simple_web_calendar(request):
     context = dict()
     web_calendar = WebCalendar(year=2019)
-    context['calendar'] = mark_safe(web_calendar.print_calendar_table(3))
+    if 'month' in request.GET:
+        month = (lambda a: a if a > 0 else 1)(request.GET.get('month'))
+        context['calendar'] = mark_safe(web_calendar.print_calendar_table(month))
+    else:
+        context['calendar'] = mark_safe(web_calendar.print_calendar_table(1))
     return render(request, 'lisg_calendar/main.html', context)

a4968c8523dabd6914cfed9ce8938381d078b47e
tronze <0183535663@naver.com>
2019-02-15 19:56:10 +0900

    Account > Admin > Registeration Email: 1. Fixed subject

--- account/admin.py
+++ account/admin.py
@@ -46,7 +46,7 @@ class UserAdmin(BaseUserAdmin):
         users = queryset
         for user in users:
             link = RegisterLink.objects.create(user=user)
-            user.email_user("[멋쟁이 사자처럼] 서강 멋사 커뮤니티 가입 안내.", "안녕하세요, %s님!\n서강대학교 멋쟁이 사자처럼에 오신걸 진심으로 환영합니다!\n\n1시간 안에 아래 링크를 통해 서강대학교 멋쟁이 사자처럼 사이트에 가입해주세요:\n%s\n\n감사합니다." % (user.name, request.build_absolute_uri(reverse('register', kwargs={'uuid': link.uuid.hex}))), settings.DEFAULT_FROM_EMAIL)
+            user.email_user("[멋쟁이 사자처럼] 서강 멋사 커뮤니티 가입 안내", "안녕하세요, %s님!\n서강대학교 멋쟁이 사자처럼에 오신걸 진심으로 환영합니다!\n\n1시간 안에 아래 링크를 통해 서강대학교 멋쟁이 사자처럼 사이트에 가입해주세요:\n%s\n\n감사합니다." % (user.name, request.build_absolute_uri(reverse('register', kwargs={'uuid': link.uuid.hex}))), settings.DEFAULT_FROM_EMAIL)
         self.message_user(request, "%s명에게 초대 메일이 성공적으로 발송되었습니다." % users.count())
 
     admin_send_invitation.short_description = "선택된 사용자(들)에게 초대 메일 발송"

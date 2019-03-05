d1624193c94d4b32c9e4ab7b24cca2b87fdaeed7
tronze <0183535663@naver.com>
2019-03-06 07:51:10 +0900

    Account > Urls: Profile url added

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 
-from .views import RegisterView
+from .views import RegisterView, MyView
 
 # Create your urls here.
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/<uuid>/', RegisterView.as_view(), name='register'),
+    path('profile/', login_required(MyView.as_view()), name='profile'),
 ]

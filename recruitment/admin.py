34499e2a1e91ecb38b5fab0cb8a40cec58ab47cb
tronze <0183535663@naver.com>
2019-03-06 16:55:02 +0900

    Recruitment > Admin: Evaluation added to admin console

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from recruitment.models import Applicant, Application, Question, Answer, ApplicantApplication
+from recruitment.models import Applicant, Application, Question, Answer, ApplicantApplication, Evaluation
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(Application)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(ApplicantApplication)
+admin.site.register(Evaluation)

53acaaba314af2ecc7f34474d3e3d682461c9db3
tronze <0183535663@naver.com>
2019-03-06 11:02:45 +0900

    LikeLion Calendar > Views, Template: Web calendar update

--- lisg_calendar/views.py
+++ lisg_calendar/views.py
@@ -9,6 +9,7 @@ from main_calendar.web_calendar import WebCalendar
 def display_simple_web_calendar(request):
     context = dict()
     web_calendar = WebCalendar(year=2019, month=2)
+    calendar = web_calendar.get_calendar_table()
     context['info'] = mark_safe(web_calendar.get_info_div().create_element())
-    context['calendar'] = mark_safe(web_calendar.get_calendar_table().create_element())
+    context['calendar'] = mark_safe(calendar.create_element())
     return render(request, 'lisg_calendar/main.html', context)

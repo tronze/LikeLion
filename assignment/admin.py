85244885b5c03d92842e2f4e390c3f60ce9bc45a
tronze <0183535663@naver.com>
2019-03-20 14:20:16 +0900

    Assignment admin update

--- assignment/admin.py
+++ assignment/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from assignment.models import Assignment, Submit, Question, Answer
+from assignment.models import Assignment, Submit, Question, Answer, SubmitImage
 
 # Register your models here.
 
 admin.site.register(Assignment)
 admin.site.register(Submit)
+admin.site.register(SubmitImage)
 admin.site.register(Question)
 admin.site.register(Answer)

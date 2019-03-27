06c3ef05f73f7b58e31772a7b18625420413a053
tronze <0183535663@naver.com>
2019-03-27 17:20:50 +0900

    Assignment Admin: Assignment Submit Information added

--- assignment/admin.py
+++ assignment/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from assignment.models import Assignment, Submit, Question, Answer, SubmitImage
+from assignment.models import Assignment, Submit, Question, Answer, SubmitImage, AssignmentSubmitInformation
 
 # Register your models here.
 
 admin.site.register(Assignment)
+admin.site.register(AssignmentSubmitInformation)
 admin.site.register(Submit)
 admin.site.register(SubmitImage)
 admin.site.register(Question)
